758c27da6faa6798433f2a27d2f37398c44172b9
Elie Marot <elie.marot@gmail.com>
2015-08-19 12:36:26 +0200

    ajout de la class reveiws

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+  has_many :reviews, :dependent => :destroy
+  validates :name, presence: true
+
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+   message: "%{value} is not a valid category" }
 end
